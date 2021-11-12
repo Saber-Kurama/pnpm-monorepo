704610b117af88bf894b731a2b702ae54a224470
guochao <463561248@qq.com>
2021-11-12 18:30:09 +0800

    first commit

---
+++ packages/a/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: saber
+ * @Date: 2021-11-12 18:27:04
+ * @LastEditTime: 2021-11-12 18:27:05
+ * @LastEditors: saber
+ * @Description: 
+ */
+console.log('a')
---
+++ packages/b/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: saber
+ * @Date: 2021-11-12 18:27:11
+ * @LastEditTime: 2021-11-12 18:27:11
+ * @LastEditors: saber
+ * @Description: 
+ */
+console.log('b')
