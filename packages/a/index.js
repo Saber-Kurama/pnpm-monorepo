f6ba60349b84c5cf7953edf51bbd29bfb62e32a6
guochao <463561248@qq.com>
2021-11-12 20:06:01 +0800

    更新a的一个版本

--- packages/a/index.js
+++ packages/a/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: saber
  * @Date: 2021-11-12 18:27:04
- * @LastEditTime: 2021-11-12 19:55:53
+ * @LastEditTime: 2021-11-12 20:05:27
  * @LastEditors: saber
  * @Description: 
  */
-console.log('a123123')
+console.log('a123123sssss')
